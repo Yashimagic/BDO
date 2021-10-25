5606aa3161d06a2dea4a127d54f5c0fee077c0db
Yashima <homotatinotudoi@gmail.com>
2021-10-25 23:44:30 +0900

    calc func add

--- BdoEnchantSimulator/bdo_enchant_simulator.py
+++ BdoEnchantSimulator/bdo_enchant_simulator.py
@@ -12,6 +12,22 @@ import tkinter as tk
 CHALLENGE_NUM = 10
 PROBABILITY = 10 #percent
 
+def run_button_click():
+    #入力バリデーション
+    
+    #取得
+    challenge_num = float(ntry_entry.get())
+    default_probability = float(prob_entry.get())
+    
+    #確率計算
+    result_prob =  calc_probability(challenge_num,default_probability)
+    result_entry.insert(0, str(result_prob) + u"%")
+    
+    
+def calc_probability(num, probability):
+    all_false_prob = np.power(((100.0 - probability) / 100.0), num)
+    result = math.floor((1.0 - all_false_prob) * 100)
+    return result
 
 all_false_prob = np.power(((100.0 - PROBABILITY) / 100.0),CHALLENGE_NUM)
 result = math.floor((1.0 - all_false_prob) * 100)
@@ -21,18 +37,29 @@ root = tk.Tk()
 root.title(u"確率シミュレータ")
 root.geometry("600x400")
 
-#ラベル
-ntry_label = tk.Label(text=u'N回チャレンジするまでに成功する確率')
-ntry_label.pack()
-ntry_label = tk.Label(text=u'N:')
-ntry_label.pack()
+header_label = tk.Label(text=u"N回チャレンジするまでに成功する確率")
+header_label.pack()
 
-#エントリー
-EditBox = tk.Entry()
-EditBox.pack()
+ntry_label = tk.Label(text=u"N : ")
+ntry_label.pack()
+ntry_entry = tk.Entry()
+ntry_entry.pack()
+
+prob_label = tk.Label(text=u"初期確率 : ")
+prob_label.pack()
+prob_entry = tk.Entry()
+prob_entry.pack()
 
 #ボタン
-run_button = ｔｋ.Button(text=u'実行', width = 20,height = 10)
+run_button = ｔｋ.Button(text=u"実行", width = 20,height = 1, command = run_button_click)
 run_button.pack()
+
+#結果
+result_label = tk.Label(text=u"結果 : ")
+result_label.pack()
+result_entry = tk.Entry()
+result_entry.pack()
+
 # ウインドウ状態の維持
 root.mainloop() 
+
